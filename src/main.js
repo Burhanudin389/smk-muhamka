b2987105b874d49c57b07ccdf623ea46492e8de8
Burhanudin Miroj <mirojburhanudin2@gmail.com>
2022-07-31 07:38:19 +0700

    :tada: Setup default config for vueJS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '../routes/router'
+import "@/css/tailwind.css"
 
-createApp(App).mount('#app')
+createApp(App)
+.use(router)
+.mount('#app')
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <img src="@/assets/banner-example.png" alt="">
+</template>
+
+<style>
+</style>
+
+<script>
+    export default {
+        mounted() {
+            // SET PAGE TITLE
+            document.title = "Beranda - SMK MUHAMMADIYAH KAJEN";  
+        },
+    }
+</script>
