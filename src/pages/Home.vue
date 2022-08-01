b609480429ba00c8a3dd1be1c85034b7547f18bd
Burhanudin Miroj <mirojburhanudin2@gmail.com>
2022-08-01 13:27:08 +0700

    Test swiper

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,27 +2,64 @@
     <!-- START : HEADER -->
     <header class="w-full relative h-custom-header z-10">
         <div class="w-full h-full max-w-screen-2xl mx-auto relative flex flex-col-reverse lg:flex-row justify-center lg:justify-between items-center">
-            <div class="w-full max-w-screen-custom mx-auto absolute z-20 lg:left-1/2 lg:transform lg:-translate-x-1/2 p-8 md:p-4">
+            <div class="w-full max-w-screen-custom mx-auto absolute z-20 lg:left-1/2 lg:transform lg:-translate-x-1/2 p-8 md:p-4 xl:p-0">
                 <h1 class="text-4xl md:text-5xl max-w-lg">SMK Muhammadiyah Kajen</h1>
                 <p class="font-light max-w-md mt-5">Sekolah dengan akreditasi A, mempunyai visi Islami, Kompeten, dan Berprestasi</p>
                 <router-link to="/" class="flex justify-center items-center w-40 h-12 rounded-md transition-all duration-300 bg-slate-300 hover:bg-slate-400 mt-5">Tentang kami</router-link>
             </div>
             <div class="w-full h-full md:w-8/12 absolute right-0 top-0 p-4 md:p-0">
-                <img src="@/assets/banner-example.png" class="w-full h-full object-cover rounded-lg md:rounded-none" alt="">
+                <swiper 
+                    :modules="modules"
+                    :loop="true"
+                    :pagination="{ clickable: true }"
+                    :autoplay="{ delay: 3500, disableOnInteraction: false }">
+                    <swiper-slide>
+                        <div class="w-full h-full">
+                            <img src="@/assets/slide-1.png" class="w-full h-full object-cover rounded-lg md:rounded-none" alt="">
+                        </div>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <div class="w-full h-full">
+                            <img src="@/assets/banner-example.png" class="w-full h-full object-cover rounded-lg md:rounded-none" alt="">
+                        </div>
+                    </swiper-slide>
+                    <swiper-slide>
+                        <div class="w-full h-full">
+                            <img src="@/assets/banner-example.png" class="w-full h-full object-cover rounded-lg md:rounded-none" alt="">
+                        </div>
+                    </swiper-slide>
+                </swiper>
             </div>
         </div>
     </header>
     <!-- END : HEADER -->
-    <div class="w-full h-screen px-4">
-        
+    <div class="w-full px-4">
+
     </div>
 </template>
 
 <script>
+    import SwiperClass, { Pagination } from 'swiper'
+    import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+
+    import 'swiper/css'
+    import 'swiper/css/pagination'
+
     export default {
+        components: {
+            Swiper,
+            SwiperSlide
+        },
+        setup() {
+            return {
+                modules: [
+                    Pagination
+                ]
+            }
+        },
         mounted() {
             // SET PAGE TITLE
-            document.title = "Beranda - SMK MUHAMMADIYAH KAJEN";  
+            document.title = "Beranda - SMK MUHAMMADIYAH KAJEN";          
         },
     }
 </script>
--- src/partials/Navbar.vue
+++ src/partials/Navbar.vue
@@ -17,38 +17,97 @@
     </div>
     <nav class="w-full px-4 sticky z-30 top-0 left-0 bg-white border border-b transition-all duration-300">
         <div class="w-full max-w-screen-custom mx-auto flex justify-between items-center h-20 md:h-24 transition-all duration-300">
-            <router-link to="/">
-                <img src="@/assets/logo.png" alt="Logo SMK" class="w-40 lg:w-48" />
-            </router-link>
+            <div class="flex items-center">
+                <button v-on:click="openNavbar()" class="mr-3 lg:hidden transition-all duration-500 transform hover:scale-125">
+                    <Icon icon="codicon:menu" class="text-2xl" />
+                </button>
+                <router-link to="/">
+                    <img src="@/assets/logo.png" alt="Logo SMK" class="w-36 lg:w-48" />
+                </router-link>
+            </div>
             <!-- SIDEBAR CLOSER -->
             <div v-on:click="closeNavbar()" v-bind:class="{'opacity-0 -z-10 invisible': !showMenu, 'z-10 opacity-100': showMenu}" class="transition-all duration-300 w-full h-screen bg-black bg-opacity-50 fixed inset-0 lg:hidden"></div>
-            <ul v-bind:class="{'transform -translate-x-full lg:transform-none': !showMenu, 'translate-x-0': showMenu}" class="fixed z-30 transition-all duration-300 flex lg:items-center flex-col lg:flex-row bg-white w-72 lg:w-auto top-0 left-0 lg:relative h-screen lg:h-auto lg:space-x-4 uppercase font-light text-sm">
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/">Beranda</router-link>
+            <ul id="sidebar-menu" v-bind:class="{'transform -translate-x-full lg:transform-none': !showMenu, 'translate-x-0': showMenu}" class="fixed z-30 transition-all duration-300 flex lg:items-center flex-col lg:flex-row bg-white w-72 md:w-80 overflow-y-auto lg:overflow-hidden lg:w-auto top-0 left-0 lg:relative h-screen lg:h-auto lg:space-x-4 lg:uppercase font-light lg:text-sm p-3 space-y-1 lg:space-y-0">
+                <div class="w-full flex items-center justify-between pb-4 mt-2">
+                    <li class="lg:hidden">
+                        <img src="@/assets/logo.png" alt="Logo SMK" class="w-36 lg:w-48" />
+                    </li>
+                    <button v-on:click="closeNavbar()" class="w-10 h-10 flex lg:hidden items-center justify-center rounded-full transition-all duration-300 bg-slate-200 hover:bg-slate-300 hover:shadow-lg focus:bg-slate-400 focus:ring-2 focus:ring-slate-200">
+                        <Icon icon="ep:close" class="text-xl" />
+                    </button>
+                </div>
+                <!--  -->
+                <div class="w-full rounded-md bg-slate-100 p-3 text-sm lg:hidden">
+                    <p class="text-center py-3 font-medium">Kunjungi juga</p>
+                    <div class="w-full mt-3">
+                        <router-link to="/" class="w-full flex justify-center py-2 rounded-md transition-all duration-300 bg-green-800 hover:bg-green-900 focus:ring-2 focus:ring-green-500 text-white">
+                            <span>I-Smart</span>
+                        </router-link>
+                    </div>
+                    <!-- <hr class="my-3"/> -->
+                    <div class="w-full mt-3">
+                        <router-link to="/" class="w-full flex justify-center py-2 rounded-md transition-all duration-300 bg-yellow-500 hover:bg-yellow-600 focus:ring-2 focus:ring-yellow-500 text-white">
+                            <span>E-Lib Muhamka</span>
+                        </router-link>
+                    </div>
+                </div>
+                <li class="lg:hidden pt-5 pb-3 font-medium">
+                    <h3>Menu utama</h3>
                 </li>
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/about">Jurusan</router-link>
+                <!--  -->
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/" class="flex items-center">
+                        <Icon icon="flat-color-icons:home" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Beranda</span>
+                    </router-link>
                 </li>
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/about">Profil</router-link>
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/about" class="flex items-start">
+                        <Icon icon="fxemoji:diamondshapewithdotinside" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Jurusan</span>
+                    </router-link>
                 </li>
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/about">Gukar</router-link>
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/about" class="flex items-start">
+                        <Icon icon="flat-color-icons:about" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Profil</span>
+                    </router-link>
                 </li>
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/about">Siswa</router-link>
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/about" class="flex items-start">
+                        <Icon icon="noto:woman-teacher-light-skin-tone" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Gukar</span>
+                    </router-link>
                 </li>
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/about">Artikel</router-link>
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/about" class="flex items-start">
+                        <Icon icon="noto:student-light-skin-tone" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Siswa</span>
+                    </router-link>
                 </li>
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/about">Galeri</router-link>
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/about" class="flex items-start">
+                        <Icon icon="flat-color-icons:news" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Artikel</span>
+                    </router-link>
                 </li>
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/about">Lowongan</router-link>
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/about" class="flex items-start">
+                        <Icon icon="flat-color-icons:gallery" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Galeri</span>
+                    </router-link>
                 </li>
-                <li v-on:click="closeNavbar()">
-                    <router-link :class="{ 'text-green-900' : active }" to="/about">Kontak</router-link>
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/about" class="flex items-start">
+                        <Icon icon="icon-park:handbag" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Lowongan</span>
+                    </router-link>
+                </li>
+                <li id="menu-item" v-on:click="closeNavbar()" class="p-2 lg:p-0 hover:bg-green-100 lg:hover:bg-transparent rounded-md">
+                    <router-link :class="{ 'text-green-900' : active }" to="/about" class="flex items-start">
+                        <Icon icon="flat-color-icons:business-contact" class="text-xl mr-2 lg:hidden" />
+                        <span class="text-sm">Kontak</span>
+                    </router-link>
                 </li>
                 <li class="hidden lg:flex">
                     <!-- MODAL CLOSER -->
@@ -78,9 +137,6 @@
                         <Icon icon="bx:search-alt" class="text-xl" />
                     </button>
                 </div>
-                <button v-on:click="openNavbar()" class="lg:hidden transition-all duration-500 transform hover:scale-125">
-                    <Icon icon="codicon:menu" class="text-2xl" />
-                </button>
             </div>
         </div>
     </nav>
@@ -111,7 +167,7 @@
             })
         },
         components: {
-            Icon
+            Icon,
         },
         data() {
             return {
